b456e2c4d1bc5f26aff4a6b524b3213a5486956e
samih <samih1079@gmail.com>
2020-10-12 10:13:46 +0300

    Initial commit

---
+++ app/src/main/java/abbas/samih/samihtasky2020/MainActivity.java
@@ -0,0 +1,14 @@
+package abbas.samih.samihtasky2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
